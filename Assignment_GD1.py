991c4be137f1790c04aefeeaadeed8a8e147c758
Nguyen Vu Nhut <nguyenvunhut256@gmail.com>
2025-09-29 08:54:39 +0700

    Thêm yêu cầu vào file

---
+++ Assignment_GD1.py
@@ -0,0 +1,11 @@
+# yêu cầu của asm
+# 1. Nhập danh sách nhân viên từ bàn phím, lưu thông tin vào file
+# 2. Đọc thông tin nhân viên từ file và xuất thông tin nhân viên ra màn hình.
+# 3. Tìm và hiển thị nhân viên theo mã nhập từ bàn phím
+# 4. Xóa nhân viên theo mã nhập từ bàn phím. Cập nhật vào file data
+# 5. Cập nhật thông tin nhân viên theo mã nhập từ bàn phím. Cập nhật vào file data
+# 6. Tìm các nhân viên theo khoảng lương nhập từ bàn phím.
+# 7. Sắp xếp nhân viên theo họ và tên
+# 8. Xắp xếp nhân viên theo thu nhập
+# 9. Xuất 5 nhân viên có thu nhập cao nhất
+
